d54a127ed77f357ef0ec508a3501251b162a2844
Julius Rakow <julius@familie-rakow.de>
2018-02-15 23:41:34 +0100

    make struct fields public

--- src/structs.rs
+++ src/structs.rs
@@ -1,59 +1,59 @@
 pub struct Message {
-	header : Message_Header,
-	records : Vec<Set>,
+	pub header : Message_Header,
+	pub records : Vec<Set>,
 }
 
 pub struct Message_Header {
-	version_number : u16,
-	length : u16,
-	export_time : u32,
-	sequence_number : u32,
-	observation_domain_id : u32,
+	pub version_number : u16,
+	pub length : u16,
+	pub export_time : u32,
+	pub sequence_number : u32,
+	pub observation_domain_id : u32,
 }
 
 pub struct Field_Specifier {
-	information_element_id : u16,
-	field_length : u16,
-	enterprise_number : Option<u32>,
+	pub information_element_id : u16,
+	pub field_length : u16,
+	pub enterprise_number : Option<u32>,
 }
 
 pub struct Set {
-	header : Set_Header,
-	records : Vec<Record>,
+	pub header : Set_Header,
+	pub records : Vec<Record>,
 }
 
 pub struct Set_Header {
-	set_id : u16,
-	length : u16,
+	pub set_id : u16,
+	pub length : u16,
 }
 
-enum Record {
+pub enum Record {
 	Template_Record,
 	Options_Template_Record,
 	Data_Record,
 }
 
 pub struct Template_Record {
-	header : Template_Record_Header,
-	fields : Vec<Field_Specifier>,
+	pub header : Template_Record_Header,
+	pub fields : Vec<Field_Specifier>,
 }
 
 pub struct Template_Record_Header {
-	template_id : u16,
-	field_count : u16,
+	pub template_id : u16,
+	pub field_count : u16,
 }
 
 pub struct Options_Template_Record {
-	header : Options_Template_Record_Header,
-	fields : Vec<Field_Specifier>,
+	pub header : Options_Template_Record_Header,
+	pub fields : Vec<Field_Specifier>,
 }
 
 pub struct Options_Template_Record_Header {
-	template_id : u16,
-	field_count : u16,
-	scope_field_count : u16,
+	pub template_id : u16,
+	pub field_count : u16,
+	pub scope_field_count : u16,
 }
 
 pub struct Data_Record {
-	fields : Vec<u8>,
+	pub fields : Vec<u8>,
 }

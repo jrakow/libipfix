e399bd8ce6e887debe4936a79f256ed468cde488
Julius Rakow <julius@familie-rakow.de>
2018-02-17 01:09:53 +0100

    reorganize structs

--- src/structs.rs
+++ src/structs.rs
@@ -1,8 +1,13 @@
-pub struct Message {
+pub const MESSAGE_HEADER_LENGTH : u16 = 16;
+pub const SET_HEADER_LENGTH : u16 = 4;
+
+#[derive(Debug, PartialEq)]
+pub struct Message<'a> {
 	pub header : Message_Header,
-	pub sets : Vec<Set>,
+	pub sets : Vec<(Set_Header, &'a[u8])>,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Message_Header {
 	pub version_number : u16,
 	pub length : u16,
@@ -11,49 +16,48 @@ pub struct Message_Header {
 	pub observation_domain_id : u32,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Field_Specifier {
 	pub information_element_id : u16,
 	pub field_length : u16,
 	pub enterprise_number : Option<u32>,
 }
 
-pub struct Set {
-	pub header : Set_Header,
-	pub records : Records,
-}
-
+#[derive(Debug, PartialEq)]
 pub struct Set_Header {
 	pub set_id : u16,
 	pub length : u16,
 }
 
-pub enum Records {
-	Template_Records(Vec<Template_Record>),
-	Options_Template_Records(Vec<Options_Template_Record>),
-	Data_Records(Vec<Data_Record>),
-}
-
+#[derive(Debug, PartialEq)]
 pub struct Template_Record {
 	pub header : Template_Record_Header,
 	pub fields : Vec<Field_Specifier>,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Template_Record_Header {
 	pub template_id : u16,
 	pub field_count : u16,
 }
 
+/* TODO
 pub struct Options_Template_Record {
 	pub header : Options_Template_Record_Header,
 	pub fields : Vec<Field_Specifier>,
+	pub scope_fields : Vec<Field_Specifier>
 }
+*/
 
+/* TODO
 pub struct Options_Template_Record_Header {
 	pub template_id : u16,
 	pub field_count : u16,
 	pub scope_field_count : u16,
 }
+*/
 
+#[derive(Debug, PartialEq)]
 pub struct Data_Record {
 	pub fields : Vec<u8>,
 }

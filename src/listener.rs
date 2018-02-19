981ac55fd2fe7df0c6738a249bccaa80a1f2161e
Julius Rakow <julius@familie-rakow.de>
2018-02-19 19:44:04 +0100

    improve listener output

--- src/listener.rs
+++ src/listener.rs
@@ -22,20 +22,27 @@ pub fn main() {
 
 	let mut message_num = 0;
 	while input != &b""[..] {
+		let mut set_num = 0;
+
 		let result = message_parser(&input[..]).unwrap();
 		input = result.0;
 		let message = result.1;
 
-		debug!("message {}: {:?}", message_num, message);
-		message_num += 1;
+		debug!("message {}: {:?}", message_num, message.header);
 
 		for (set_header, data) in message.sets {
 			match set_header.set_id {
 				2 | 3 => {
 					let templates = template_records_parser(data, set_header).unwrap().1;
-					for template in templates {
-						cache.update_with(template);
+					for template in &templates {
+						cache.update_with(template.clone());
 					}
+					trace!(
+						"message {}: set {}: {:?}",
+						message_num,
+						set_num,
+						(set_header, templates)
+					);
 				}
 				256...0xffff => {
 					let template = match cache.lookup(set_header.set_id) {
@@ -50,13 +57,21 @@ pub fn main() {
 						data_records_parser(data, set_header.length - SET_HEADER_LENGTH, template)
 							.ok();
 
-					if opt.is_none() || opt.as_ref().unwrap().0 == &b""[..] {
+					if opt.is_none() || opt.as_ref().unwrap().0 != &b""[..] {
 						error!("failed to parse data set");
 						continue;
 					}
+					trace!(
+						"message {}: set {}: {:?}",
+						message_num,
+						set_num,
+						(set_header, opt.unwrap().1)
+					);
 				}
 				id => error!("received set with reserved set id {}", id),
 			}
+			set_num += 1;
 		}
+		message_num += 1;
 	}
 }
